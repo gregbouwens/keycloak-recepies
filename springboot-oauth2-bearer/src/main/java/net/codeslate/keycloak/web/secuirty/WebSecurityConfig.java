a17f790e178fc4fa01db5ed6ac4e7b8b5dd92b52
Greg Bouwens <greg.bouwens@hanselware.com>
2022-08-03 19:08:42 -0700

    Got this working locally. Not sure what that means but it's working with Postman - getting results back every time.

--- springboot-oauth2-bearer/src/main/java/net/codeslate/keycloak/web/secuirty/WebSecurityConfig.java
+++ springboot-oauth2-bearer/src/main/java/net/codeslate/keycloak/web/secuirty/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package net.codeslate.keycloak.web.secuirty;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
